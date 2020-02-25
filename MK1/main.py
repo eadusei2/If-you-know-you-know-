3ac76d1982d00ccdb8efd45e1d6077117c935916
Evans Adusei <eadusei2@asu.edu>
2020-02-24 22:51:16 -0700

    Added Capabilities
    Signed-off-by: Evans Adusei <eadusei2@asu.edu>

--- MK1/main.py
+++ MK1/main.py
@@ -1,5 +1,5 @@
-# Github sucks balls
-
+import MySQLdb
+from MySQLdb import _mysql
 from tkinter import *
 
 
@@ -10,7 +10,7 @@ def exit_screen():
 def log_on():
     global main_entry
     """"""
-# ----------------- login/registration screen ------
+    # ----------------- login/registration screen ------
     main_entry = Tk()
 
     main_entry.geometry("1200x800")
@@ -29,16 +29,17 @@ def log_on():
     Button(text="Register", height="8", width="80", command=registration).pack()
 
     # exit button
-    button = Button(text="Exit", height="4", width="20",command=exit_screen)
+    button = Button(text="Exit", height="4", width="20", command=exit_screen)
     button.pack()
 
     main_entry.mainloop()
     log_on()
     login_screen()
+    #login_authentication()
 
 
 def registration():
-
+    global username, password
     registration_gui = Toplevel(main_entry)
     registration_gui.title("Registration")
     registration_gui.geometry("600x350")
@@ -73,53 +74,99 @@ def registration():
     Button(registration_gui, text="Register", width=20, height=2).pack()
 
     # exit button
-    button1 = Button(registration_gui,text="Exit", height="2", width="10", command=exit_screen)
+    button1 = Button(registration_gui, text="Exit", height="2", width="10", command=exit_screen)
     button1.pack()
 
     # --------------------------------------------------------------------------------------------
 
 
 def login_screen():
-        global username_verify, password_verify
+    global username_l, password_l, password_entry_l, username_entry_l, login_gui
 
-        login_gui = Toplevel(main_entry)
-        login_gui.title("Login")
-        login_gui.geometry("600x350")
-        login_gui.configure(bg='gray37')
+    login_gui = Toplevel(main_entry)
+    login_gui.title("Login")
+    login_gui.geometry("600x350")
+    login_gui.configure(bg='gray37')
 
-        username = StringVar()
-        password = StringVar()
+    username_l = StringVar()
+    password_l = StringVar()
 
-        Label(login_gui, text="Please enter your information", bg="turquoise", width=100, height=2).pack()
-        Label(login_gui, text="", bg='gray37').pack()
+    Label(login_gui, text="Please enter your information", bg="turquoise", width=100, height=2).pack()
+    Label(login_gui, text="", bg='gray37').pack()
 
-        username_lable = Label(login_gui, text="Username")
-        username_lable.pack()
+    username_lable = Label(login_gui, text="Username")
+    username_lable.pack()
 
-        username_entry = Entry(login_gui, textvariable=username, width=40)
-        username_entry.pack()
+    username_entry = Entry(login_gui, textvariable=username_l, width=40)
+    username_entry.pack()
 
-        # space between username and password
-        Label(login_gui, text="", bg='gray37').pack()
+    # space between username and password
+    Label(login_gui, text="", bg='gray37').pack()
 
-        # Set password label
-        password_lable = Label(login_gui, text="Password")
-        password_lable.pack()
+    # Set password label
+    password_lable = Label(login_gui, text="Password")
+    password_lable.pack()
 
-        # Set password entry
-        password_entry = Entry(login_gui, textvariable=password, width=40)
-        password_entry.pack()
+    # Set password entry
+    password_entry = Entry(login_gui, textvariable=password_l, width=40)
+    password_entry.pack()
 
-        Label(login_gui, text="", bg='gray37').pack()
+    Label(login_gui, text="", bg='gray37').pack()
 
-        # Set register button
-        Button(login_gui, text="Login", width=20, height=2).pack()
+    # Set register button
+    Button(login_gui, text="Login", width=20, height=2, command=login_authentication).pack()
+    Label(login_gui, text="", bg='gray37').pack()
 
-        # exit button
-        button1 = Button(login_gui, text="Exit", height="2", width="10", command=exit_screen)
-        button1.pack()
+    # exit button
+    button1 = Button(login_gui, text="Exit", height="2", width="10", command=exit_screen)
+    button1.pack()
 
-        # -----------------------------------------------------------------------------------------
+    # -----------------------------------------------------------------------------------------
+
+
+def login_authentication():
+    global user, passw, checkU, checkp
+
+    try:
+        user = username_l.get()
+        passw = password_l.get()
+
+        password_entry_l.delete(0, END)
+        username_entry_l.delete(0, END)
+    except:
+        pass
+
+    try:
+        db_connection = MySQLdb.connect("107.180.1.16", "lascoronas", "!!Lascoronas", "lascoronas")
+        # If connection is not successful
+    except:
+        print("Can't connect to database")
+        return 0
+    # If Connection Is Successful
+    print("Connected")
+
+    # Making Cursor Object For Query Execution
+    cursor = db_connection.cursor()
+
+    # Executing Query
+    cursor.execute(f"SELECT username, password From User;")
+    #cursor.execute(f"SELECT * From User WHERE username='{user}' AND password='{passw}';")
+
+    data = cursor.fetchall()
+    print(data)
+
+    for d in data:
+        checkU = d[0]
+        checkp = d[1]
+
+    if checkU == username_l.get() and checkp == password_l.get():
+                Label(login_gui, text="'Successful login'", bg='gray37').pack()
+    elif checkU != username_l.get() or checkp != password_l.get():
+                Label(login_gui, text="'Unsuccessful login'", bg='gray37').pack()
+
+    db_connection.close()
+
+
 
 
 if __name__ == '__main__':
