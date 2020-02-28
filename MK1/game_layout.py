e6f42e43bd3370d76507f8600780388ba78a1044
Evans Adusei <eadusei2@asu.edu>
2020-02-28 00:38:01 -0700

    Added Capabilities
    Signed-off-by: Evans Adusei <eadusei2@asu.edu>

--- MK1/game_layout.py
+++ MK1/game_layout.py
@@ -1,35 +1,94 @@
 from tkinter import *
+import MySQLdb
+from MySQLdb import _mysql
+import random
 
-win = Tk()
-win.geometry("1200x800")
-win.config(bg='dark turquoise')
 
-username = StringVar()
-username_label = Label(win, textvariable=username, bg='white', width='20', height='1')
-username_label.grid(row=0, column=0, sticky=NW, padx=85)
+def game_screen():
+    global choice_ID, question_Sen, choice1, choice2, choice3, choice4
 
-Label(win, text='Username:', height=1).grid(row=0, sticky=NW)
+    try:
+        db_connection = MySQLdb.connect("107.180.1.16", "lascoronas", "!!Lascoronas", "lascoronas")
+        # If connection is not successful
+    except:
+        print("Can't connect to database")
+        return 0
+        # If Connection Is Successful
+    print("Connected")
 
-question = StringVar()
-question_label = Label(win, textvariable=question, background='white', width='100', height='5')
-question_label.grid(row=0, columnspan=2, padx=120, pady=100)
+    # Making Cursor Object For Query Execution
+    cursor = db_connection.cursor()
 
-Label(win, text='Question:').grid(row=0, sticky=W, padx=10)
+    # Executing Query
+    cursor.execute(f"select distinct qc.choice_id, q.question, qc.correct_choice, qc.choice1, "
+                   f"qc.choice2, qc.choice3, qc.choice4 from question_choices qc inner join questions q "
+                   f"on qc.question_id=q.question_id inner join question_category qct "
+                   f"on qct.cat_id = qc.cat_id and q.cat_id = qc.cat_id where qc.cat_id=5;")
+    # the genre category will go in brackets
+    """    cursor.execute(f"select distinct qc.choice_id, q.question, qc.correct_choice, qc.choice1, "
+                       f"qc.choice2, qc.choice3, qc.choice4 from question_choices qc inner join questions q "
+                       f"on qc.question_id=q.question_id inner join question_category qct "
+                       f"on qct.cat_id = qc.cat_id and q.cat_id = qc.cat_id"
+                       f"where qc.cat_id={};" )"""
 
-answer = StringVar()
-answer_1 = Button(win, textvariable=answer, background='gray37', width=40, height='4')
-answer_1.grid(row=1, column=0, sticky=E)
+    info = cursor.fetchall()
+    data = random.choice(info)
 
-answer2 = StringVar()
-answer_2 = Button(win, textvariable=answer2, bg='gray37', width=40, height='4')
-answer_2.grid(row=1, column=1, sticky=W)
+    # holds the question ID
+    choice_ID = data[0]
 
-answer3 = StringVar()
-answer_3 = Button(win, textvariable=answer3, bg='gray37', width=40, height='4')
-answer_3.grid(row=2, column=0, sticky=E)
+    # holds the question sentence
+    question_Sen = data[1]
+    # correct choice
+    correct_choice = data[2]
+    # 4 options
+    choice1 = data[3]
+    choice2 = data[4]
+    choice3 = data[5]
+    choice4 = data[6]
 
-answer4 = StringVar()
-answer_4 = Button(win, textvariable=answer4, bg='gray37', width=40, height='4')
-answer_4.grid(row=2, column=1, sticky=W)
 
-win.mainloop()
+    print(question_Sen, choice1,choice2,choice3, choice4)
+
+    win = Tk()
+    win.geometry("1200x800")
+    win.config(bg='dark turquoise')
+
+    username = StringVar()
+    username_label = Label(win, textvariable=username, bg='white', width='20', height='3')
+    username_label.grid(row=0, column=0, sticky=NW, padx=85)
+
+    Label(win, text='Username:', height=3).grid(row=0, sticky=NW)
+
+    question = StringVar()
+    question.set(question_Sen)
+    question_label = Label(win, textvariable=question, background='white', width='120', height='8', font=40)
+    question_label.grid(row=0, columnspan=2, padx=120, pady=100)
+
+    #Label(win, text='Question:', height=3).grid(row=0, sticky=W, padx=10)
+
+    choice_1 = StringVar()
+    choice_1.set(choice1)
+    choice1 = Button(win, textvariable=choice_1, background='gray37', width=60, height='6', font=40, fg='dark turquoise')
+    choice1.grid(row=1, column=0, sticky=E)
+
+    choice_2 = StringVar()
+    choice_2.set(choice2)
+    choice2 = Button(win, textvariable=choice_2, bg='gray37', width=60, height='6',font=40, fg='dark turquoise')
+    choice2.grid(row=1, column=1, sticky=W)
+
+    choice_3 = StringVar()
+    choice_3.set(choice3)
+    choice3 = Button(win, textvariable=choice_3, bg='gray37', width=60, height='6',font=40, fg='dark turquoise')
+    choice3.grid(row=2, column=0, sticky=E)
+
+    choice_4 = StringVar()
+    choice_4.set(choice4)
+    choice4 = Button(win, textvariable=choice_4, bg='gray37', width=60, height='6',font=40, fg='dark turquoise')
+    choice4.grid(row=2, column=1, sticky=W)
+
+    win.mainloop()
+
+
+if __name__ == '__main__':
+    game_screen()
