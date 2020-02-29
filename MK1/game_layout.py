244bc82f93f9b86f2cf7bf8ff81ec78dddab0fdc
Evans Adusei <eadusei2@asu.edu>
2020-02-28 21:31:22 -0700

    Added Capabilities
    Signed-off-by: Evans Adusei <eadusei2@asu.edu>

--- MK1/game_layout.py
+++ MK1/game_layout.py
@@ -3,6 +3,7 @@ import MySQLdb
 from MySQLdb import _mysql
 import random
 import json
+from tkinter import ttk
 
 start = list()
 
@@ -13,9 +14,24 @@ def background_music():
         play_obj = wave_obj.play()
         play_obj.wait_done()
 
+
+def callbackFunc(event):
+    print("New Element Selected")
+
+
 def game_screen():
     global choice_ID, question_Sen, choice1, choice2, choice3, choice4, \
         choice_1, choice_2, choice_3, choice_4,correct_choice
+
+    if clicked.get() == 'Comedy':
+        categoryOpp = '2'
+    elif clicked.get() == 'Romance':
+        categoryOpp = '3'
+    elif clicked.get() == 'Thriller':
+        categoryOpp = '4'
+    elif clicked.get() == 'Action':
+        categoryOpp = '5'
+    print(categoryOpp)
 
     try:
         db_connection = MySQLdb.connect("107.180.1.16", "lascoronas", "!!Lascoronas", "lascoronas")
@@ -30,16 +46,15 @@ def game_screen():
     cursor = db_connection.cursor()
 
     # Executing Query
-    cursor.execute(f"select distinct qc.choice_id, q.question, qc.correct_choice, qc.choice1, "
-                   f"qc.choice2, qc.choice3, qc.choice4 from question_choices qc inner join questions q "
-                   f"on qc.question_id=q.question_id inner join question_category qct "
-                   f"on qct.cat_id = qc.cat_id and q.cat_id = qc.cat_id where qc.cat_id=5;")
-    # the genre category will go in brackets
     """    cursor.execute(f"select distinct qc.choice_id, q.question, qc.correct_choice, qc.choice1, "
                        f"qc.choice2, qc.choice3, qc.choice4 from question_choices qc inner join questions q "
                        f"on qc.question_id=q.question_id inner join question_category qct "
-                       f"on qct.cat_id = qc.cat_id and q.cat_id = qc.cat_id"
-                       f"where qc.cat_id={};" )"""
+                       f"on qct.cat_id = qc.cat_id and q.cat_id = qc.cat_id where qc.cat_id=5;")"""
+    # the genre category will go in brackets
+    cursor.execute(f"select distinct qc.choice_id, q.question, qc.correct_choice, qc.choice1, "
+                       f"qc.choice2, qc.choice3, qc.choice4 from question_choices qc inner join questions q "
+                       f"on qc.question_id=q.question_id inner join question_category qct "
+                       f"on qct.cat_id = qc.cat_id and q.cat_id = qc.cat_id where qc.cat_id={categoryOpp};")
 
     info = cursor.fetchall()
     with open('question.json', 'r') as file:
@@ -78,7 +93,6 @@ def game_screen():
     choice3 = data['QUESTIONS'][0]['choice3']
     choice4 = data['QUESTIONS'][0]['choice4']
 
-
     print(question_Sen, choice1,choice2,choice3, choice4)
 
     win = Tk()
@@ -93,7 +107,7 @@ def game_screen():
 
     question = StringVar()
     question.set(question_Sen)
-    question_label = Label(win, textvariable=question, background='white', width='120', height='8', font=40)
+    question_label = Label(win, textvariable=f'{question}', background='white', width='120', height='8', font=40)
     question_label.grid(row=0, columnspan=2, padx=120, pady=100)
 
     #Label(win, text='Question:', height=3).grid(row=0, sticky=W, padx=10)
@@ -121,10 +135,54 @@ def game_screen():
     win.mainloop()
 
 
+def game_menu():
+    global select_genre, log_out, game_button, categoryOpp, clicked, test1
+
+    categoryOpp ='2'
+    win = Tk()
+    win.geometry("1200x800")
+    win.config(bg='dark turquoise')
+
+    # Opening screen of program: homepage/menu page
+    frame1 = Frame(win, bg='gray37')
+    frame1.pack(side=TOP, anchor=E)
+
+    frame2 = Frame(win, bg='gray37')
+    frame2.place(x=0, y=0)
+
+    frame3 = Frame(win, bg='gray37')
+    frame3.pack(side=TOP, anchor=N)
+
+    select_genre_label = Label(frame3, text='Select your genre:', font=("Calibri", 15), bg='gray37', fg='white')
+    select_genre_label.pack()
+
+    genres = ['Genres', 'Comedy', 'Romance', 'Thriller', 'Action']
+    clicked = StringVar()
+    clicked.set('Genres')
+    #select_genre = ttk.Combobox(frame3, values=['Comedy', 'Romance', 'Thriller', 'Action'])
+    select_genre = OptionMenu(frame3, clicked, *genres)
+    select_genre.config(width=10, height=2, font=("Calibri", 40))
+    select_genre.config(font=("Calibri", 15))
+    select_genre.pack()
+    #select_genre.bind("<Button-1>", callbackFunc)
+
+    log_out = Button(frame1, text='Log Out', font=("Calibri", 10), height=1, width=8)
+    log_out.pack(side=TOP, anchor=E, padx=10, pady=10)
+
+    leaderboard_button = Button(frame2, text='Leaderboard', font=("Calibri", 10), height=1, width=12)
+    leaderboard_button.pack(side=TOP, anchor=W, padx=10, pady=10)
+
+    game_button = Button(win, text='PLAY GAME', font=("Calibri", 20), height=3, width=15, command=game_screen)
+    game_button.pack(side=TOP, anchor=N, pady=200)
+
+    win.mainloop()
+
+def show():
+    print("Selected value :", clicked.get())
+
 def correct_pick1():
     if choice_1.get() == correct_choice:
         print('Correct pick test complete')
-
     elif choice_1.get() != correct_choice:
         print('wrong answer test complete')
 
@@ -132,7 +190,6 @@ def correct_pick1():
 def correct_pick2():
     if choice_2.get() == correct_choice:
         print('Correct pick test complete')
-
     elif choice_2.get() != correct_choice:
         print('wrong answer test complete')
 
@@ -140,7 +197,6 @@ def correct_pick2():
 def correct_pick3():
     if choice_3.get() == correct_choice:
         print('Correct pick test complete')
-
     elif choice_3.get() != correct_choice:
         print('wrong answer test complete')
 
@@ -148,11 +204,9 @@ def correct_pick3():
 def correct_pick4():
     if choice_4.get() == correct_choice:
         print('Correct pick test complete')
-
-
     elif choice_4.get() != correct_choice:
         print('wrong answer test complete')
 
 
 if __name__ == '__main__':
-    game_screen()
+    game_menu()
--- MK1/main.py
+++ MK1/main.py
@@ -1,6 +1,10 @@
 import MySQLdb
 from MySQLdb import _mysql
 from tkinter import *
+import random
+import json
+
+start = list()
 
 
 def exit_screen():
@@ -35,6 +39,8 @@ def log_on():
     main_entry.mainloop()
     log_on()
     login_screen()
+    game_menu()
+    game_screen()
 
 
 def registration():
@@ -204,44 +210,210 @@ def login_authentication():
 
     if checkU == username_l.get() and checkp == password_l.get():
                 Label(login_gui, text="'Successful login'", bg='gray37').pack()
+                game_menu()
     elif checkU != username_l.get() or checkp != password_l.get():
                 Label(login_gui, text="'Unsuccessful login'", bg='gray37').pack()
 
     db_connection.close()
 
-def game_screen():
-    win = Tk(main_entry)
-    win.geometry("1200x500")
-    win.config(bg='dark turquoise')
 
+def game_screen():
+    global choice_ID, question_Sen, choice1, choice2, choice3, choice4, \
+        choice_1, choice_2, choice_3, choice_4,correct_choice
+
+    gameScreen_Gui = Toplevel()
+    gameScreen_Gui.geometry("1200x800")
+    gameScreen_Gui.config(bg='dark turquoise')
+
+    if clicked.get() == 'Comedy':
+        categoryOpp = '2'
+    elif clicked.get() == 'Romance':
+        categoryOpp = '3'
+    elif clicked.get() == 'Thriller':
+        categoryOpp = '4'
+    elif clicked.get() == 'Action':
+        categoryOpp = '5'
+    print(categoryOpp)
+
+
+    try:
+        db_connection = MySQLdb.connect("107.180.1.16", "lascoronas", "!!Lascoronas", "lascoronas")
+        # If connection is not successful
+    except:
+        print("Can't connect to database")
+        return 0
+        # If Connection Is Successful
+    print("Connected")
+
+    # Making Cursor Object For Query Execution
+    cursor = db_connection.cursor()
+
+    # Executing Query
+    """    cursor.execute(f"select distinct qc.choice_id, q.question, qc.correct_choice, qc.choice1, "
+                       f"qc.choice2, qc.choice3, qc.choice4 from question_choices qc inner join questions q "
+                       f"on qc.question_id=q.question_id inner join question_category qct "
+                       f"on qct.cat_id = qc.cat_id and q.cat_id = qc.cat_id where qc.cat_id=5;")"""
+    # the genre category will go in brackets
+    cursor.execute(f"select distinct qc.choice_id, q.question, qc.correct_choice, qc.choice1, "
+                       f"qc.choice2, qc.choice3, qc.choice4 from question_choices qc inner join questions q "
+                       f"on qc.question_id=q.question_id inner join question_category qct "
+                       f"on qct.cat_id = qc.cat_id and q.cat_id = qc.cat_id where qc.cat_id={categoryOpp};")
+
+    info = cursor.fetchall()
+    with open('question.json', 'r') as file:
+        test1 = json.load(file)
+
+    for i in info:
+        choice_ID = f'{i[0]}'
+        question_Sen = f'{i[1]}'.strip()
+        correct_choice = f'{i[2]}'.strip()
+        choice1 = f'{i[3]}'.strip()
+        choice2 = f'{i[4]}'.strip()
+        choice3 = f'{i[5]}'.strip()
+        choice4 = f'{i[6]}'.strip()
+
+        #start.append(i)
+
+        test1 = [{"ID": choice_ID, "Question_ID": question_Sen, "correct_Choice": correct_choice, "choice1": choice1,
+                  "choice2": choice2, "choice3": choice3, "choice4": choice4}]
+
+        with open('question.json', 'w') as file:
+            start_dict = {"QUESTIONS": test1}
+            start.append(start_dict)
+
+            run = json.dump(start, file, indent=3)
+
+    with open('question.json', 'r') as file:
+            test2 = json.load(file)
+
+    data = random.choice(test2)
+    print(data)
+
+    question_Sen = data['QUESTIONS'][0]['Question_ID']
+    correct_choice = data['QUESTIONS'][0]['correct_Choice']
+    choice1 = data['QUESTIONS'][0]['choice1']
+    choice2 = data['QUESTIONS'][0]['choice2']
+    choice3 = data['QUESTIONS'][0]['choice3']
+    choice4 = data['QUESTIONS'][0]['choice4']
+
+    print(question_Sen, choice1,choice2,choice3, choice4)
+
+    # ------------------------------------------------------------------------------------------------------------------
     username = StringVar()
-    username_label = Label(win, textvariable=username, bg='white', width='20', height='1')
+    username_label = Label(gameScreen_Gui, textvariable=username, bg='white', width='20', height='3')
     username_label.grid(row=0, column=0, sticky=NW, padx=85)
 
-    Label(win, text='Username:', height=1).grid(row=0, sticky=NW)
+    Label(gameScreen_Gui, text='Username:', height=3).grid(row=0, sticky=NW)
 
     question = StringVar()
-    question_label = Label(win, textvariable=question, background='white', width='100', height='5')
+    question.set(question_Sen)
+    question_label = Label(gameScreen_Gui, textvariable=question, background='white', width='120', height='8', font=40)
     question_label.grid(row=0, columnspan=2, padx=120, pady=100)
 
-    Label(win, text='Question:').grid(row=0, sticky=W, padx=10)
+    #Label(win, text='Question:', height=3).grid(row=0, sticky=W, padx=10)
 
-    answer = StringVar()
-    answer_1 = Button(win, textvariable=answer, background='gray37', width=40, height='4')
-    answer_1.grid(row=1, column=0, sticky=E)
+    choice_1 = StringVar()
+    choice_1.set(choice1)
+    choice1 = Button(gameScreen_Gui, textvariable=choice_1, background='gray37', width=60, height='6', font=40, fg='dark turquoise', command=correct_pick1)
+    choice1.grid(row=1, column=0, sticky=E)
 
-    answer2 = StringVar()
-    answer_2 = Button(win, textvariable=answer2, bg='gray37', width=40, height='4')
-    answer_2.grid(row=1, column=1, sticky=W)
+    choice_2 = StringVar()
+    choice_2.set(choice2)
+    choice2 = Button(gameScreen_Gui, textvariable=choice_2, bg='gray37', width=60, height='6',font=40, fg='dark turquoise', command=correct_pick2)
+    choice2.grid(row=1, column=1, sticky=W)
 
-    answer3 = StringVar()
-    answer_3 = Button(win, textvariable=answer3, bg='gray37', width=40, height='4')
-    answer_3.grid(row=2, column=0, sticky=E)
+    choice_3 = StringVar()
+    choice_3.set(choice3)
+    choice3 = Button(gameScreen_Gui, textvariable=choice_3, bg='gray37', width=60, height='6',font=40, fg='dark turquoise', command=correct_pick3)
+    choice3.grid(row=2, column=0, sticky=E)
 
-    answer4 = StringVar()
-    answer_4 = Button(win, textvariable=answer4, bg='gray37', width=40, height='4')
-    answer_4.grid(row=2, column=1, sticky=W)
+    choice_4 = StringVar()
+    choice_4.set(choice4)
+    choice4 = Button(gameScreen_Gui, textvariable=choice_4, bg='gray37', width=60, height='6',font=40, fg='dark turquoise', command=correct_pick4)
+    choice4.grid(row=2, column=1, sticky=W)
+    gameScreen_Gui.update()
 
+    #gameScreen_Gui.mainloop()
+
+
+def game_menu():
+    global select_genre, log_out, game_button, categoryOpp, clicked
+
+    gameMenu_Gui = Toplevel()
+    gameMenu_Gui.geometry("1200x800")
+    gameMenu_Gui.config(bg='dark turquoise')
+
+    # Opening screen of program: homepage/menu page
+    frame1 = Frame(gameMenu_Gui, bg='gray37')
+    frame1.pack(side=TOP, anchor=E)
+
+    frame2 = Frame(gameMenu_Gui, bg='gray37')
+    frame2.place(x=0, y=0)
+
+    frame3 = Frame(gameMenu_Gui, bg='gray37')
+    frame3.pack(side=TOP, anchor=N)
+
+    select_genre_label = Label(frame3, text='Select your genre:', font=("Calibri", 15), bg='gray37', fg='white')
+    select_genre_label.pack()
+
+    genres = ['Genres', 'Comedy', 'Romance', 'Thriller', 'Action']
+    clicked = StringVar()
+    clicked.set(genres[0])
+    select_genre = OptionMenu(frame3, clicked, *genres)
+    select_genre.config(width=10, height=1, font=("Calibri", 15))
+    select_genre['menu'].config(font=("Calibri", 15))
+    select_genre.pack()
+
+    if clicked.get() == 'Comedy':
+        categoryOpp = '2'
+    elif clicked.get() == 'Romance':
+        categoryOpp = '3'
+    elif clicked.get() == 'Thriller':
+        categoryOpp = '4'
+    elif clicked.get() == 'Action':
+        categoryOpp = '5'
+
+    log_out = Button(frame1, text='Log Out', font=("Calibri", 10), height=1, width=8)
+    log_out.pack(side=TOP, anchor=E, padx=10, pady=10)
+
+    leaderboard_button = Button(frame2, text='Leaderboard', font=("Calibri", 10), height=1, width=12)
+    leaderboard_button.pack(side=TOP, anchor=W, padx=10, pady=10)
+
+    game_button = Button(gameMenu_Gui, text='PLAY GAME', font=("Calibri", 20), height=3, width=15, command=game_screen)
+    game_button.pack(side=TOP, anchor=N, pady=200)
+
+    gameMenu_Gui.update()
+
+
+    #gameMenu_Gui.mainloop()
+
+
+def correct_pick1():
+    if choice_1.get() == correct_choice:
+        print('Correct pick test complete')
+    elif choice_1.get() != correct_choice:
+        print('wrong answer test complete')
+
+
+def correct_pick2():
+    if choice_2.get() == correct_choice:
+        print('Correct pick test complete')
+    elif choice_2.get() != correct_choice:
+        print('wrong answer test complete')
+
+
+def correct_pick3():
+    if choice_3.get() == correct_choice:
+        print('Correct pick test complete')
+    elif choice_3.get() != correct_choice:
+        print('wrong answer test complete')
+
+
+def correct_pick4():
+    if choice_4.get() == correct_choice:
+        print('Correct pick test complete')
+    elif choice_4.get() != correct_choice:
+        print('wrong answer test complete')
 
 if __name__ == '__main__':
-    game_screen()
+    log_on()
